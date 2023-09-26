739ddd31d1ca92fd03e351af1d3d971e68922093
Arturito <artursa212@gmail.com>
2023-09-26 21:10:00 +0500

    Petrichenko first console application

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('3212321');
+'usegit strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const lastFilmName = +prompt('Один из просмотренных фильмов?', ''),
+    lastFilmRating = +prompt('На сколько вы оцените его?', '');
+
+personalMovieDB.movies[lastFilmName] = lastFilmRating;
