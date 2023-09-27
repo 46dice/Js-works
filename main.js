6f613b4043a963e668794b44bb68b9b54248d957
Arturito <artursa212@gmail.com>
2023-09-27 23:20:51 +0500

    user interaction with checks/Условия и циклы практика ч.2

--- main.js
+++ main.js
@@ -8,9 +8,29 @@ const personalMovieDB = {
     genres: [],
     privat: false,
 };
-const lastFilmName = +prompt('Один из просмотренных фильмов?', ''),
-    lastFilmRating = +prompt('На сколько вы оцените его?', '');
 
-personalMovieDB.movies[lastFilmName] = lastFilmRating;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из просмотренных фильмов?', ''),
+        b = prompt('На сколько вы оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('WELL DONE');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Мало фильмов, бро');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert('Классическое количество просмотров фильмов, бро');
+} else if (personalMovieDB.count > 30) {
+    alert('Прям дохуя фильмов посмотрел, бро, красавчик!');
+} else {
+    alert('Произошла ошибка')
+}
+
 
 console.log(personalMovieDB);
